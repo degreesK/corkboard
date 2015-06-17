2bf94ecea056a99b5ccfaa8e89ab76f447871712
Tania Kelvin <tkdegrees@gmail.com>
2015-06-17 13:26:37 -0700

    images begin edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616205547) do
+ActiveRecord::Schema.define(version: 20150617201806) do
 
   create_table "corks", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 20150616205547) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "post_file_name"
+    t.string   "post_content_type"
+    t.integer  "post_file_size"
+    t.datetime "post_updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
